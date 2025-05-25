98e1d72c2d8f66876b29f995b53466074341d203
Joás dos Santos <joas.santos@prinzcapital.com.br>
2025-05-25 13:41:26 -0300

    Updated version

--- Test.py
+++
@@ -1,5 +0,0 @@
-from algorithm import engine, read_file, txt_to_matrix
-
-if __name__ == '__main__':
-    fixed_operation = txt_to_matrix('fixed_operations.txt')
-    print(f"Fixed operations: {fixed_operation}")
